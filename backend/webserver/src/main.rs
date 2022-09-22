7f2ffe83889675eb1b388b79c853423961354220
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-09-22 12:13:42 +0200

    remove unused code

--- backend/webserver/src/main.rs
+++ backend/webserver/src/main.rs
@@ -1,16 +1,4 @@
 use std::env;
-
-
-fn set_env() -> u16{
-    let key = "$PORT";
-
-    match env::var(key){
-        Ok(val) => val.parse::<u16>().unwrap(),
-        _ => 8080
-    }
-}
-
-
 #[macro_use] extern crate rocket;
 
 #[get("/")]
