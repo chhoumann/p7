30aa51c65ab76847c6a6a21fbff4be6b6568255e
Ivikhostrup <ihostr16@student.aau.dk>
2022-09-22 11:15:38 +0200

    pushing to the world

--- backend/webserver/src/main.rs
+++ backend/webserver/src/main.rs
@@ -1,4 +1,34 @@
-fn main() {
-    let port = env!("PORT");
-    println!("Exposed port is: {}", port);
+use actix_web::{web, App, HttpServer, Result};
+use serde::Deserialize;
+use std::env;
+
+
+fn set_env() -> u16{
+    let key = "PORT";
+
+    match env::var(key){
+        Ok(val) => val.parse::<u16>().unwrap(),
+        _ => 8080
+    }
+}
+
+
+#[derive(Deserialize)]
+struct Code {
+    code: String,
+}
+
+async fn index(code: web::Json<Code>) -> Result<String> {
+    Ok(format!("Welcome {}!", code.code))
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    let env_port = set_env();
+    HttpServer::new(|| App::new().route("/haskell", web::post().to(index)))
+        .bind(("127.0.0.1", env_port))?
+        .run()
+        .await
 }
