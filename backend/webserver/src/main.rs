f8f4584f7e9d93687338f7502b7f86d08744df55
Ivikhostrup <ihostr16@student.aau.dk>
2022-09-22 13:02:02 +0200

    Update main.rs

--- backend/webserver/src/main.rs
+++ backend/webserver/src/main.rs
@@ -1,4 +1,26 @@
 use rocket::serde::{Deserialize, json::Json};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -20,5 +42,5 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index]).mount("/", routes![new])
+    rocket::build().mount("/", routes![index]).mount("/", routes![new]).attach(CORS)
 }
