cb48cf2f73f1abb388a4b3cd2aec75df761f9edb
Pattrigue <pfas19@student.aau.dk>
2022-09-29 12:30:42 +0200

    Fixes to Haskell endpoint

--- backend/webserver/src/endpoints/haskell.rs
+++ backend/webserver/src/endpoints/haskell.rs
@@ -7,12 +7,11 @@ use crate::services::{code_runner, write_struct};
 #[post("/haskell", format="json", data = "<task>")]
 pub fn new(task: Json<Task>) -> Json<Task>{ 
     let json_task = Json(Task { code: task.code.to_string() }).into_inner();
-    println!("dfsfds");
-    let out_file = "../services/haskell-code/gfdjkslghdfskl";
-    let _ = write_struct::write_struct(json_task, out_file);
-    let res = code_runner::compile_file(out_file, ".hs").unwrap();
-    println!("abc");
+    let out_file = "haskell-code/test.hs";
     
+    write_struct::write_struct(json_task, out_file).expect("Could not write code to file!");
+
+    let res = code_runner::compile_file("test", ".hs").unwrap();
 
     Json(Task { code: res })
  }
--- backend/webserver/src/services/write_struct.rs
+++ backend/webserver/src/services/write_struct.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use crate::domain::task::Task;
 
-pub fn write_struct(exercise : Task, file_name: &str) -> std::io::Result<()>{    
+pub fn write_struct(exercise : Task, file_name: &str) -> std::io::Result<()> { 
     let mut file = File::create(file_name)?;
     file.write_all(exercise.code.as_bytes())?;
     Ok(())
