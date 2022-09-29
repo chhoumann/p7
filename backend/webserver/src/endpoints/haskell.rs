586da851caef664b201da8fa16efc2296b5bccd5
Pattrigue <pfas19@student.aau.dk>
2022-09-29 12:59:50 +0200

    Refactoring

--- backend/webserver/src/endpoints/haskell.rs
+++ backend/webserver/src/endpoints/haskell.rs
@@ -2,15 +2,22 @@ use rocket::serde::{json::Json};
 use crate::domain::task::Task;
 use crate::domain::code_runner_result::CodeRunnerResult;
 use crate::services::{code_runner, write_struct};
+use std::path::Path;
+
+const FILE_NAME : &str = "test";
+const FILE_EXTENSION : &str = ".hs";
 
 #[post("/haskell", format="json", data = "<task>")]
 pub fn new(task: Json<Task>) -> Json<CodeRunnerResult> { 
     let json_task = Json(Task { code: task.code.to_string() }).into_inner();
-    let out_file = "haskell-code/test.hs";
+    let out_file = Path::new("haskell-code").join(format!("{}{}", FILE_NAME, FILE_EXTENSION))
+        .into_os_string()
+        .into_string()
+        .unwrap();
     
-    write_struct::write_struct(json_task, out_file).expect("Could not write code to file!");
+    write_struct::write_struct(json_task, &out_file).expect("Could not write code to file!");
 
-    let output = code_runner::compile_file("test", ".hs").unwrap();
+    let output = code_runner::compile_file(FILE_NAME, FILE_EXTENSION).unwrap();
 
     return Json(CodeRunnerResult { output: output })
  }
