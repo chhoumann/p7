3ed0c8ad9385096170f1dabec5613f71f7bbc09a
Ivikhostrup <ihostr16@student.aau.dk>
2022-10-20 15:00:44 +0200

    Update [id].tsx

--- frontend/src/pages/syllabus/[id].tsx
+++ frontend/src/pages/syllabus/[id].tsx
@@ -1,37 +1,55 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router'
-import { prisma } from '../../server/db/client'
-import React from 'react'
+import { trpc } from "../../utils/trpc"
+import React, { useState } from 'react'
 
 
-const SyllabusId: NextPage = async () => {
+const SyllabusId: NextPage = () => {
+
+    const [selectedId, setSelectedId] = useState<string>()
 
     const router = useRouter();
-    const {id} = router.query;
-    const sessions = await prisma.
+    const { id } = router.query;
+    //const sessions = trpc.useQuery
+    const testData = [
+        { name: "hello", id: "i21en" },
+        { name: "Jesus", id: "i23en" },
+        { name: "Christ", id: "i22en" },
+    ]
 
 
     return (
         <div className='container flex justify-center items-center w-full h-[80vh]'>
-            <div className='flex flex-col bg-gray-500 bg-opacity-75 mt-40 w-[50vh] h-full border-solid border-2 border-red-700'>
-                <SessionRow name={"hello"} id={"Test"}/>
+            <div className='flex flex-col mt-40 w-[60vh] h-full border-solid border-2 border-gray-500'>
+
+                {testData.map((session) =>
+                    <React.Fragment key={session.id}>
+                        <SessionRow {...session} onClick={() => setSelectedId(session.id)} isSelected={session.id === selectedId}/>
+                    </React.Fragment>
+                )}
+                <div className='mb-auto' />
+                <div className='flex flex-row gap-4 mx-3 my-3'>
+                    <button className='bg-gray-300 px-3 py-2 hover:bg-gray-400 hover:outline hover:outline-2 hover:outline-black'>
+                        Create new session
+                    </button>
+                    <button className='bg-gray-300 px-3 py-2 hover:bg-gray-400 hover:outline hover:outline-2 hover:outline-black'>
+                        Edit
+                    </button>
+                </div>
             </div>
-            
         </div>
     )
 }
 
-//grid h-screen justify-center
-// grid-cols-1 w-96 top-5
-export default SyllabusId; 
+export default SyllabusId;
 
 
-function SessionRow({id, name}:{id: string, name: string}) {
+function SessionRow({ id, name, isSelected, onClick }:{ id: string, name: string, isSelected: boolean, onClick: () => void }) {
 
 
     return (
-        <div className='bg-red-400'>
-            <span>This is true</span>
+        <div className={`p-3 m-3 ${isSelected?'bg-gray-400':"bg-gray-300 hover:bg-gray-400"}`} onClick={onClick}>
+            <span>{name}</span>
         </div>
     )
 }
