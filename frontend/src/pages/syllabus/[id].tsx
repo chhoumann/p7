316d32eeeae4a0f64a60710b382f260dc01050b6
Ivikhostrup <ihostr16@student.aau.dk>
2022-10-26 13:26:43 +0200

    Update [id].tsx

--- frontend/src/pages/syllabus/[id].tsx
+++ frontend/src/pages/syllabus/[id].tsx
@@ -15,12 +15,20 @@ const SyllabusId: NextPage = () => {
         { name: "hello", id: "i21en" },
         { name: "Jesus", id: "i23en" },
         { name: "Christ", id: "i22en" },
+        { name: "Christ", id: "i24en" },
+        { name: "Christ", id: "i25en" },
+        { name: "Christ", id: "i26en" },
+        { name: "Christ", id: "i27en" },
+        { name: "Christ", id: "i28en" },
+        { name: "Christ", id: "i29en" },
+        { name: "Christ", id: "i30en" },
+        { name: "Christ", id: "i31en" },
     ]
 
 
     return (
-        <div className='container flex justify-center items-center w-full h-[80vh]'>
-            <div className='flex flex-col mt-40 w-[60vh] h-full border-solid border-2 border-gray-500'>
+        <div className='container flex justify-center items-center w-full h-[75vh]'>
+            <div className='flex flex-col mt-40 w-[60vh] h-full border-solid border-2 border-gray-500 overflow-auto'>
 
                 {testData.map((session) =>
                     <React.Fragment key={session.id}>
@@ -28,7 +36,7 @@ const SyllabusId: NextPage = () => {
                     </React.Fragment>
                 )}
                 <div className='mb-auto' />
-                <div className='flex flex-row gap-4 mx-3 my-3'>
+                <div className='flex flex-row gap-4 mx-3 my-3 pt-3 pb-3 sticky bottom-0 bg-white'>
                     <button className='bg-gray-300 px-3 py-2 hover:bg-gray-400 hover:outline hover:outline-2 hover:outline-black'>
                         Create new session
                     </button>
