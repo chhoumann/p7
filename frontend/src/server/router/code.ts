ef81bb6e5d0d26764d32d0365108f62111000e20
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-10-26 13:41:19 +0200

    demo spam router

--- frontend/src/server/router/code.ts
+++ frontend/src/server/router/code.ts
@@ -5,40 +5,74 @@ import { env } from "../../env/server.mjs";
 import * as trpc from "@trpc/server";
 
 export const codeRouter = createRouter()
-    .mutation("haskell", {
-        input: z.object({
-            code: z.string()
-        }),
-        output: z.object({
+  .mutation("haskell", {
+    input: z.object({
+      code: z.string(),
+    }),
+    output: z.object({
+      success: z.boolean(),
+      result: z.string(),
+    }),
+    async resolve({ input }) {
+      try {
+        const webserverResponse = await ky
+          .post(`${env.WEBSERVER_ADDRESS}/haskell`, {
+            json: { code: input.code },
+          })
+          .json();
+
+        const parsedResponse = z
+          .object({
+            result: z.string(),
             success: z.boolean(),
-            result: z.string()
-        }),
-        async resolve({ input }) {
-            try {
-                const webserverResponse = await ky
-                    .post(`${env.WEBSERVER_ADDRESS}/haskell`, {
-                        json: { code: input.code },
-                    })
-                    .json();
+          })
+          .safeParse(webserverResponse);
 
-                const parsedResponse = z
-                .object({
-                    result: z.string(),
-                    success: z.boolean(),
-                })
-                .safeParse(webserverResponse);
-
-                if (!parsedResponse.success) {
-                    return { success: false, result: parsedResponse.error.message };
-                }
-
-                return parsedResponse.data;
-            } catch (error) {
-                throw new trpc.TRPCError({
-                    code: "INTERNAL_SERVER_ERROR",
-                    message: "Could not make request to code runner.",
-                    cause: error
-                })
-            }
+        if (!parsedResponse.success) {
+          return { success: false, result: parsedResponse.error.message };
         }
-    })
+
+        return parsedResponse.data;
+      } catch (error) {
+        throw new trpc.TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Could not make request to code runner.",
+          cause: error,
+        });
+      }
+    },
+  })
+  .query("spam", {
+    input: z.object({
+      count: z.number(),
+      code: z.string(),
+    }),
+    async resolve({ input }) {
+      console.time("spam");
+
+      const ps = Array(input.count)
+        .fill(undefined)
+        .map(() => {
+          try {
+            return ky
+              .post(`${env.WEBSERVER_ADDRESS}/haskell`, {
+                json: { code: input.code },
+              })
+              .json();
+          } catch {
+            return undefined;
+          }
+        });
+
+      const responses = await Promise.all(ps);
+      const fulfilledCount = responses.filter((x) => x !== undefined).length;
+
+      console.timeEnd("spam");
+      
+      return {
+        requests: input.count,
+        fulfilled: fulfilledCount,
+        responses,
+      }
+    },
+  });
