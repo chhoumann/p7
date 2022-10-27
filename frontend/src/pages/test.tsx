a8489015db086c00b52c2967cf36df50f90223f1
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-10-27 11:59:33 +0200

    specify test request count in input

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { trpc } from "../utils/trpc";
 
-function TestPage() {
-  const code = `
+const code = `
 module Code where
 add x y = x + y
 `;
 
-  const test = `
+const test = `
 import Test.Hspec
 import Test.QuickCheck
 import Code (add)
@@ -19,44 +18,72 @@ main = hspec $ do
       add 2 2 \`shouldBe\` (4 :: Int)
 `;
 
-  const req = trpc.useQuery(
-    [
-      "code.spam",
-      {
-        count: 10,
-        code,
-        test,
-      },
-    ],
-    {
-      enabled: false,
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-    }
-  );
+function TestPage() {
+    const [count, setCount] = useState(10);
+    const [countError, setCountError] = useState(false);
 
-  if (req?.data?.responses) console.table(req.data.responses);
+    const req = trpc.useQuery(
+        [
+            "code.spam",
+            {
+                count,
+                code,
+                test,
+            },
+        ],
+        {
+            enabled: false,
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+        }
+    );
 
-  return (
-    <div>
-      {req.isSuccess && req.data && (
+    return (
         <div>
-          Sent {req.data.requestCount} requests to the server.
-          <br />
-          Received {req.data.fulfilled} responses. This took {req.data.duration}
-          ms.
-        </div>
-      )}
+            {req.isSuccess && req.data && (
+                <div>
+                    Sent {req.data.requestCount} requests to the server.
+                    <br />
+                    Received {req.data.fulfilled} responses. This took{" "}
+                    {req.data.duration}
+                    ms.
+                </div>
+            )}
 
-      <button
-        className="rounded px-4 py-2 bg-gray-300"
-        onClick={() => req.refetch()}
-      >
-        Run
-      </button>
-    </div>
-  );
+            {req.isLoading && <div>Loading...</div>}
+
+            {countError && <div>Count is invalid.</div>}
+
+            <div className="flex gap-4">
+                <button
+                    className="rounded px-4 py-2 bg-gray-300"
+                    onClick={() => req.refetch()}
+                >
+                    Run
+                </button>
+
+                <input
+                    onInput={(input) => {
+                        const count = parseInt(input.currentTarget.value);
+
+                        if (!count) {
+                            setCountError(true);
+                            return;
+                        }
+
+                        setCountError(false);
+                        setCount(count);
+                    }}
+                    className="border-2"
+                    type="number"
+                    name="count"
+                    placeholder="10"
+                    defaultValue={10}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default TestPage;
