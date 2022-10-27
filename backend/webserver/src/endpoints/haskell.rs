942fa314d6b52e4f92fd2e8c7387f31d90e59433
Pattrigue <pfas19@student.aau.dk>
2022-10-27 15:14:05 +0200

    pain with little gain

--- backend/webserver/src/endpoints/haskell.rs
+++ backend/webserver/src/endpoints/haskell.rs
@@ -1,17 +1,38 @@
+use rocket::State;
 use rocket::serde::{json::Json};
 use crate::domain::exercise_submission_request::ExerciseSubmissionRequest;
 use crate::domain::code_runner_result::CodeRunnerResponse;
 use crate::services::test_runner;
 
+#[get("/")]
+fn index(runtime: &State<rocket::tokio::runtime::Runtime>) {
+    runtime.spawn_blocking(|| {
+
+    });
+}
 
 #[post("/haskell", format="json", data = "<exercise_submission_request>")]
-pub async fn new(exercise_submission_request: Json<ExerciseSubmissionRequest>) -> Json<CodeRunnerResponse> {
-    let exercise_submission = ExerciseSubmissionRequest 
+pub async fn new(exercise_submission_request: Json<ExerciseSubmissionRequest>, runtime: &State<tokio::runtime::Runtime>) -> Json<CodeRunnerResponse> {
+    println!("Spawning thread");
+
+    let res = runtime.spawn_blocking(|| schedule_test(exercise_submission_request))
+        .await
+        .unwrap()
+        .await;
+
+    println!("Ending thread");
+
+    return res
+ }
+
+
+async fn schedule_test(exercise_submission_request: Json<ExerciseSubmissionRequest>) -> Json<CodeRunnerResponse> {
+    let exercise_submission = ExerciseSubmissionRequest
     {
-         code: exercise_submission_request.code.to_string(),
-         test: exercise_submission_request.test.to_string()
+        code: exercise_submission_request.code.to_string(),
+        test: exercise_submission_request.test.to_string()
     };
-    
+
     let json_exercise_submission = Json(exercise_submission)
         .into_inner();
 
@@ -20,16 +41,15 @@ pub async fn new(exercise_submission_request: Json<ExerciseSubmissionRequest>) -
     let result = test_runner::execute(exercise_code, test_code).await;
 
     let output = match result {
-        Ok(output) => Json(CodeRunnerResponse { 
+        Ok(output) => Json(CodeRunnerResponse {
             success: true,
             result: output
         }),
-        Err(error_message) => Json(CodeRunnerResponse { 
-            success: false, 
+        Err(error_message) => Json(CodeRunnerResponse {
+            success: false,
             result: error_message.to_string()
         })
     };
 
-    return output
- }
-
+    return output;
+}
--- backend/webserver/src/main.rs
+++ backend/webserver/src/main.rs
@@ -4,9 +4,17 @@ mod services;
 
 #[macro_use]
 extern crate rocket;
+
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
+    let runtime = rocket::tokio::runtime::Builder::new_multi_thread()
+        .max_blocking_threads(2)
+        .enable_all()
+        .build()
+        .unwrap();
+
     let _rocket = rocket::build()
+        .manage(runtime)
         .mount("/", routes![endpoints::index::index])
         .mount("/", routes![endpoints::haskell::new])
         .attach(endpoints::cors::CORS)
