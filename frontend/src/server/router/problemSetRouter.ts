45a1be2d6e9cfa43289ed4ff07103d2def698065
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-11-02 16:19:03 +0100

    prettify code and return arr (unwrap obj)

--- frontend/src/server/router/problemSetRouter.ts
+++ frontend/src/server/router/problemSetRouter.ts
@@ -2,64 +2,69 @@ import { createRouter } from "./context";
 import { z } from "zod";
 import * as trpc from "@trpc/server";
 import { prisma } from "../db/client";
-import {ProblemSet} from "@prisma/client";
+import { ProblemSet } from "@prisma/client";
 
 const zodObjProblemSet = z.object({
-  topic: z.string(),
-  date: z.date(),
-  syllabusName: z.string(),
-})
-
-
-export const problemSetsRouter = createRouter().query("getBySyllabusId", {
-  input: z.string(),
-  async resolve({ input: name }) {
-    console.log("id is: ", name)
-    const problemSets: ProblemSet[] | null = await prisma.problemSet.findMany({
-      where: { syllabusName: name },
-    });
-
-    if (!problemSets || problemSets.length === 0) {
-      throw new trpc.TRPCError({
-        code: "NOT_FOUND",
-        message: `ProblemSets with syllabusId ${name} not found`,
-      });
-    }
-
-    return { problemSets };
-  },
-}).mutation("postProblemSet", {
-  input: zodObjProblemSet,
-  async resolve({input}) {
-    try {
-      await prisma.problemSet.create({data: input})
-      return {success: true, result: "Successfully created problem!"}
-    } catch (error) {
-      throw new trpc.TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Could not post problemset to database.",
-        cause: error
-      })
-    }
-  }
-})
-.mutation("deleteProblemSet", {
-  input: z.string(),
-  async resolve({ input: id }) {
-    try {
-      await prisma.problemSet.delete({
-        where: {
-          id,
-        },
-      });
-
-      return true;
-    } catch (error) {
-      throw new trpc.TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: `Could not delete problem set with ID ${id}`,
-        cause: error,
-      });
-    }
-  },
+    topic: z.string(),
+    date: z.date(),
+    syllabusName: z.string(),
 });
+
+export const problemSetsRouter = createRouter()
+    .query("getBySyllabusId", {
+        input: z.string(),
+        async resolve({ input: name }) {
+            console.log("id is: ", name);
+            const problemSets: ProblemSet[] | null =
+                await prisma.problemSet.findMany({
+                    where: { syllabusName: name },
+                });
+
+            if (!problemSets || problemSets.length === 0) {
+                throw new trpc.TRPCError({
+                    code: "NOT_FOUND",
+                    message: `ProblemSets with syllabusId ${name} not found`,
+                });
+            }
+
+            return problemSets;
+        },
+    })
+    .mutation("postProblemSet", {
+        input: zodObjProblemSet,
+        async resolve({ input }) {
+            try {
+                await prisma.problemSet.create({ data: input });
+                return {
+                    success: true,
+                    result: "Successfully created problem!",
+                };
+            } catch (error) {
+                throw new trpc.TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Could not post problemset to database.",
+                    cause: error,
+                });
+            }
+        },
+    })
+    .mutation("deleteProblemSet", {
+        input: z.string(),
+        async resolve({ input: id }) {
+            try {
+                await prisma.problemSet.delete({
+                    where: {
+                        id,
+                    },
+                });
+
+                return true;
+            } catch (error) {
+                throw new trpc.TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: `Could not delete problem set with ID ${id}`,
+                    cause: error,
+                });
+            }
+        },
+    });
