3c1e97747a41203809de94431f6f9bb084832ef9
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2022-11-02 11:49:26 +0100

    Added cypress

---
+++ frontend/cypress/e2e/canDoX.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Can add entities', () => {
+    beforeEach(() => {
+      
+      cy.visit('http://localhost:3000/')
+    })
+
+    it('renders home page', () => {
+
+      })
+})
