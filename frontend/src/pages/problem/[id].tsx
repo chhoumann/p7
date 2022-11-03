fb1cae7b117f1cbbe97473a3ad40864a2e3954cd
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-11-03 08:18:26 +0100

    nicer loading state for problems

--- frontend/src/pages/problem/[id].tsx
+++ frontend/src/pages/problem/[id].tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useRef, useState } from "react";
 import { trpc } from "../../utils/trpc";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 enum TabState {
     Instructions,
@@ -13,16 +13,16 @@ const SolveProblem: NextPage = () => {
     const [tab, setTab] = useState<TabState>(TabState.Instructions);
     const codebox = useRef<HTMLTextAreaElement>(null);
     const router = useRouter();
-    const {id} = router.query;
+    const { id } = router.query;
 
-    const problem = trpc.useQuery(["problem.byId", id as string],{
-        enabled: router.isReady
-    })
+    const problem = trpc.useQuery(["problem.byId", id as string], {
+        enabled: router.isReady,
+    });
     const test = trpc.useQuery(["test.byId", id as string], {
-        enabled: router.isReady
-    })
+        enabled: router.isReady,
+    });
 
-    console.log(problem.data)
+    console.log(problem.data);
     const mutation = trpc.useMutation("code.haskell", {
         onSuccess: () => setTab(TabState.Result),
     });
@@ -32,13 +32,22 @@ const SolveProblem: NextPage = () => {
         setTab(TabState.Instructions);
     }
 
-    if(!problem.isSuccess || !problem.data){
-        return <div>Could not find problem ☹</div>
+    if (problem.isLoading || test.isLoading) {
+        return (
+            <div className="flex flex-col justify-center h-screen">
+                <div className="mx-auto my-auto">Loading...</div>
+            </div>
+        );
     }
 
-    if(!test.isSuccess || !test.data){
-        return <div>Could not find test ☹</div>
+    if (!problem.isSuccess || !problem.data) {
+        return <div>Could not find problem ☹</div>;
     }
+
+    if (!test.isSuccess || !test.data) {
+        return <div>Could not find test ☹</div>;
+    }
+
     return (
         <>
             <Head>
@@ -57,11 +66,11 @@ const SolveProblem: NextPage = () => {
                 <main className="mx-auto h-full w-full">
                     <div className="flex flex-row gap-8 items-center justify-center h-full w-full px-10">
                         <div className="border-2 w-full h-3/4 flex flex-col rounded-lg">
-              <textarea
-                  ref={codebox}
-                  className="h-5/6 px-2 py-1 font-mono resize-none rounded-lg outline-0"
-                  defaultValue={problem.data.template}
-              />
+                            <textarea
+                                ref={codebox}
+                                className="h-5/6 px-2 py-1 font-mono resize-none rounded-lg outline-0"
+                                defaultValue={problem.data.template}
+                            />
                             <div className="h-1/6 w-full p-4 gap-2 items-center justify-end flex flex-row border-t">
                                 <button className="rounded-lg bg-sky-500 hover:bg-sky-400 px-4 py-2 text-white font-semibold">
                                     Attempt
@@ -70,8 +79,10 @@ const SolveProblem: NextPage = () => {
                                     className="rounded-lg bg-green-500 hover:bg-green-400 px-4 py-2 text-white font-semibold"
                                     onClick={() =>
                                         mutation.mutate({
-                                            code: codebox.current?.value ?? "test",
-                                            test: test.data.code
+                                            code:
+                                                codebox.current?.value ??
+                                                "test",
+                                            test: test.data.code,
                                         })
                                     }
                                 >
@@ -84,7 +95,9 @@ const SolveProblem: NextPage = () => {
                                 <Tab
                                     text="Instructions"
                                     selected={tab === TabState.Instructions}
-                                    onClick={() => setTab(TabState.Instructions)}
+                                    onClick={() =>
+                                        setTab(TabState.Instructions)
+                                    }
                                 />
                                 <Tab
                                     text="Results"
@@ -94,13 +107,18 @@ const SolveProblem: NextPage = () => {
                                 />
                             </div>
                             <div className="px-2 py-1 overflow-y-auto overflow-x-clip">
-                                {tab === TabState.Instructions && <Instructions text={problem.data.description}/>}
-                                {tab === TabState.Result && mutation.isSuccess && (
-                                    <Results
-                                        result={mutation.data?.result}
-                                        success={mutation.data?.success}
+                                {tab === TabState.Instructions && (
+                                    <Instructions
+                                        text={problem.data.description}
                                     />
                                 )}
+                                {tab === TabState.Result &&
+                                    mutation.isSuccess && (
+                                        <Results
+                                            result={mutation.data?.result}
+                                            success={mutation.data?.success}
+                                        />
+                                    )}
                             </div>
                         </div>
                     </div>
@@ -111,11 +129,11 @@ const SolveProblem: NextPage = () => {
 };
 
 function Tab({
-                 text,
-                 selected,
-                 disabled,
-                 onClick,
-             }: {
+    text,
+    selected,
+    disabled,
+    onClick,
+}: {
     text: string;
     selected: boolean;
     disabled?: boolean;
@@ -128,12 +146,12 @@ function Tab({
             } ${disabled ? "" : "cursor-pointer"}`}
             onClick={disabled ? () => false : onClick}
         >
-      {text}
-    </span>
+            {text}
+        </span>
     );
 }
 
-function Instructions({text}:{text:string}) {
+function Instructions({ text }: { text: string }) {
     return (
         <>
             <p>{text}</p>
@@ -143,20 +161,27 @@ function Instructions({text}:{text:string}) {
 
 function Results({ result, success }: { result?: string; success?: boolean }) {
     if (!result) return <></>;
-    const formattedResult = result.trim()
+    const formattedResult = result
+        .trim()
         .replaceAll("[v]", "✅")
-        .replaceAll("[x]", "❌")
+        .replaceAll("[x]", "❌");
 
     return (
         <div className="flex flex-col w-full">
-      <span className="w-full text-center text-3xl">
-        {success ? "Success!" : "Code failed to run"}
-      </span>
+            <span className="w-full text-center text-3xl">
+                {success ? "Success!" : "Code failed to run"}
+            </span>
 
             <div className="my-8" />
 
             <span className="text-xl">Output</span>
-            <pre className={`bg-gray-100 p-2 rounded-lg text-white ${success ? `bg-green-600 `:`bg-red-700`}`}>{formattedResult}</pre>
+            <pre
+                className={`bg-gray-100 p-2 rounded-lg text-white ${
+                    success ? `bg-green-600 ` : `bg-red-700`
+                }`}
+            >
+                {formattedResult}
+            </pre>
         </div>
     );
 }
