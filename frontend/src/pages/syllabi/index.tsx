2908f497b1421fa5b1a548fe4ac1bc028a444036
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-11-03 21:39:22 +0100

    use modal for creating new syllabi

--- frontend/src/pages/syllabi/index.tsx
+++ frontend/src/pages/syllabi/index.tsx
@@ -1,128 +1,237 @@
 import { GetServerSideProps, NextPage } from "next";
 import { trpc } from "../../utils/trpc";
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import Link from "next/link";
 import Layout from "../../components/layout";
 import { getSession } from "next-auth/react";
-import { ArrowRightCircle } from "react-feather";
+import { ArrowRightCircle, Edit, Trash } from "react-feather";
+import clsx from "clsx";
+import { Dialog, Transition } from "@headlessui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const Syllabi: NextPage<{ role: string }> = ({ role }) => {
-    const [selectedName, setSelectedName] = useState<string>();
-
     const syllabi = trpc.useQuery(["syllabus.findAll"]);
     const deleteSyllabus = trpc.useMutation(["syllabus.deleteSyllabus"], {
         onSuccess: () => syllabi.refetch(),
     });
 
-    async function handleDeleteSyllabus() {
-        if (!selectedName) return;
+    async function handleDeleteSyllabus(name: string) {
+        await deleteSyllabus.mutateAsync(name);
 
-        await deleteSyllabus.mutateAsync(selectedName);
         syllabi.refetch();
     }
 
-    return (
-        <Layout title="Syllabi">
-            <h1 className="text-4xl mt-20 text-center">Your courses</h1>
+    const [isOpen, setIsOpen] = useState(false);
 
-            <div className="my-8 mx-auto border-b w-1/2 shadow-xs" />
-            
-            <div className="container flex flex-col justify-center items-center mx-auto w-full h-3/4">
-                <div className="flex flex-col w-full max-w-3xl h-full overflow-auto">
-                    {syllabi.isSuccess &&
-                        syllabi.data.map((syllabus) => (
-                            <React.Fragment key={syllabus.name}>
-                                {role === "teacher" ? (
-                                    <SessionRowSelect
-                                        {...syllabus}
-                                        onClick={() =>
-                                            setSelectedName(syllabus.name)
-                                        }
-                                        isSelected={
-                                            syllabus.name === selectedName
-                                        }
-                                    />
-                                ) : (
-                                    <SessionRow name={syllabus.name} />
-                                )}
-                            </React.Fragment>
-                        ))}
-                    <div className="mb-auto" />
-                    {role === "teacher" ? (
-                        <div className="flex flex-row-reverse justify-center gap-4 mx-3 my-3 pt-3 pb-3 sticky bottom-0 bg-white">
-                            <Link href={`/syllabi/create`}>
-                                <button className="bg-gray-300 px-3 py-2 hover:bg-gray-400 hover:outline hover:outline-2 hover:outline-black">
+    return (
+        <React.Fragment>
+            <CreateSyllabusModal
+                isOpen={isOpen}
+                setIsOpen={(b) => setIsOpen(b)}
+                onSuccess={() => syllabi.refetch()}
+            />
+            <Layout title="Syllabi">
+                <h1 className="text-4xl mt-20 text-center">Your courses</h1>
+
+                <div className="my-8 mx-auto border-b w-1/2 shadow-xs" />
+
+                <div className="container flex flex-col justify-center items-center mx-auto w-full h-3/4">
+                    <div className="flex flex-col w-full max-w-3xl h-full overflow-auto">
+                        {syllabi.isSuccess &&
+                            syllabi.data.map((syllabus) => (
+                                <React.Fragment key={syllabus.name}>
+                                    {role === "teacher" ? (
+                                        <SyllabusRowSelect
+                                            {...syllabus}
+                                            onClickTrash={() => {
+                                                handleDeleteSyllabus(
+                                                    syllabus.name
+                                                );
+                                            }}
+                                        />
+                                    ) : (
+                                        <SyllabusRow name={syllabus.name} />
+                                    )}
+                                </React.Fragment>
+                            ))}
+                        <div className="mb-auto" />
+                        {role === "teacher" ? (
+                            <div className="flex flex-row-reverse justify-center gap-4 mx-3 my-3 pt-3 pb-3 sticky bottom-0 bg-white">
+                                {/* <Link href={`/syllabi/create`}> */}
+                                <button
+                                    className="px-3 py-2 outline outline-1 outline-gray-400 rounded-lg hover:ring-1 hover:ring-offset-white hover:ring-gray-400 hover:ring-offset-2 transition duration-300 ease-in-out"
+                                    onClick={() => setIsOpen(true)}
+                                >
                                     Create new syllabus
                                 </button>
-                            </Link>
-
-                            <Link
-                                href={
-                                    selectedName
-                                        ? `/syllabi/edit/${selectedName}`
-                                        : `/syllabi`
-                                }
-                            >
-                                <button className="bg-gray-300 px-3 py-2 hover:bg-gray-400 hover:outline hover:outline-2 hover:outline-black">
-                                    Edit
-                                </button>
-                            </Link>
-                            <Link
-                                href={
-                                    selectedName
-                                        ? `/syllabi/${selectedName}`
-                                        : `/syllabi`
-                                }
-                            >
-                                <button className="bg-gray-300 px-3 py-2 hover:bg-gray-400 hover:outline hover:outline-2 hover:outline-black">
-                                    View
-                                </button>
-                            </Link>
-                            <button
-                                className="bg-red-300 px-3 py-2 hover:bg-gray-400 hover:outline hover:outline-2 hover:outline-black"
-                                onClick={handleDeleteSyllabus}
-                            >
-                                Delete
-                            </button>
-                        </div>
-                    ) : null}
+                                {/* </Link> */}
+                            </div>
+                        ) : null}
+                    </div>
                 </div>
-            </div>
-        </Layout>
+            </Layout>
+        </React.Fragment>
     );
 };
 
 export default Syllabi;
 
-function SessionRowSelect({
+function SyllabusRowSelect({
     name,
-    isSelected,
-    onClick,
+    onClickTrash,
 }: {
     name: string;
-    isSelected: boolean;
-    onClick: () => void;
+    onClickTrash: () => void;
 }) {
     return (
         <div
-            className={`p-3 m-3 text-xl border-b-2 ${
-                isSelected ? "bg-gray-200" : "hover:bg-gray-200"
-            }`}
-            onClick={onClick}
+            className={clsx(
+                "p-3 m-3 text-xl group hover:bg-zinc-100 rounded-lg flex flex-row align-middle justify-between transition-all duration-500 ease-in-out"
+            )}
         >
             <span>{name}</span>
+            <div className="flex flex-row gap-3">
+                <Trash
+                    size={30}
+                    className="cursor-pointer"
+                    onClick={onClickTrash}
+                />
+                <Link href={`/syllabi/edit/${name}`}>
+                    <Edit size={30} className="cursor-pointer" />
+                </Link>
+                <Link href={`/syllabi/${name}`}>
+                    <ArrowRightCircle size={30} className="cursor-pointer" />
+                </Link>
+            </div>
         </div>
     );
 }
 
-function SessionRow({ name }: { name: string }) {
+function SyllabusRow({ name }: { name: string }) {
     return (
-        <div className="p-3 m-3 text-xl group hover:bg-slate-50 rounded-lg flex flex-row align-middle justify-between transition-all duration-500 ease-in-out">
-            <span>{name}</span>
-            <Link href={`/syllabi/${name}`}>
-                <ArrowRightCircle size={30} className="cursor-pointer group-hover:scale-125 transition-transform duration-500 ease-in-out" />
-            </Link>
-        </div>
+        <Link href={`/syllabi/${name}`}>
+            <div
+                className={
+                    "p-3 m-3 text-xl group hover:bg-zinc-100 rounded-lg flex flex-row align-middle cursor-pointer justify-between transition-all duration-500 ease-in-out"
+                }
+            >
+                <span>{name}</span>
+                <ArrowRightCircle
+                    size={30}
+                    className="cursor-pointer group-hover:scale-125 transition-transform duration-500 ease-in-out"
+                />
+            </div>
+        </Link>
+    );
+}
+
+function CreateSyllabusModal({
+    isOpen,
+    setIsOpen,
+    onSuccess,
+}: {
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
+    onSuccess: () => void;
+}) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset
+    } = useForm<{ title: string }>();
+
+    const mutation = trpc.useMutation(["syllabus.postSyllabus"], {
+        onSuccess: () => {
+            setIsOpen(false);
+            onSuccess();
+            reset();
+        },
+    });
+
+    const onSubmit: SubmitHandler<{ title: string }> = (data) => {
+        try {
+            mutation.mutate({
+                name: data.title,
+            });
+        } catch (error) {} // Handled by form library
+    };
+
+    return (
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog
+                as="div"
+                className="relative z-10"
+                onClose={() => setIsOpen(false)}
+            >
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                <Dialog.Title
+                                    as="h3"
+                                    className="text-lg font-medium leading-6 text-gray-900"
+                                >
+                                    New syllabus
+                                </Dialog.Title>
+                                <div className="container flex justify-center items-center w-full">
+                                    <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+                                        <div className="mt-4">
+                                            <input
+                                                type="text"
+                                                placeholder="Title"
+                                                {...register("title", {
+                                                    required: true,
+                                                })}
+                                                className="w-full rounded-lg border border-gray-500 py-2 px-3 text-grey-darkest"
+                                            />
+                                            {!!errors.title ? (
+                                                <span>
+                                                    Invalid input: {errors.title.type}
+                                                </span>
+                                            ) : null}
+
+                                            {mutation.isError ? (
+                                                <span>
+                                                    {mutation.error.message}
+                                                </span>
+                                            ) : null}
+                                        </div>
+                                        <div className="mb-auto" />
+                                        <div className="flex flex-row justify-end gap-4 pt-3 pb-3 sticky bottom-0 bg-white">
+                                            <input
+                                                type="submit"
+                                                className="px-3 py-2 outline outline-1 outline-gray-400 rounded-lg hover:ring-1 cursor-pointer hover:ring-offset-white hover:ring-gray-400 hover:ring-offset-2 transition duration-300 ease-in-out"
+                                            />
+                                        </div>
+                                    </form>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
     );
 }
 
