916a889d71103d8f8e483b9c45b0e0000ea77c0e
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-11-03 15:01:14 +0100

    use new backend api

--- frontend/src/server/router/code.ts
+++ frontend/src/server/router/code.ts
@@ -4,79 +4,151 @@ import ky from "ky";
 import { env } from "../../env/server.mjs";
 import * as trpc from "@trpc/server";
 
-export const codeRouter = createRouter()
-  .mutation("haskell", {
-    input: z.object({
-      code: z.string(),
-      test: z.string()
-    }),
-    output: z.object({
-      success: z.boolean(),
-      result: z.string(),
-    }),
-    async resolve({ input }) {
-      try {
-        const webserverResponse = await ky
-          .post(`${env.WEBSERVER_ADDRESS}/haskell`, {
-            json: { code: input.code, test: input.test},
-          })
-          .json();
+function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-        const parsedResponse = z
-          .object({
-            result: z.string(),
+const jobSubmitResponseSchema = z.object({
+    id: z.string(),
+});
+
+const jobResultResponseSchema = z
+    .object({
+        status: z.enum(["in progress", "not found"]),
+    })
+    .or(
+        z.object({
+            status: z.string().regex(/complete/),
             success: z.boolean(),
-          })
-          .safeParse(webserverResponse);
+            output: z.string(),
+        })
+    );
 
-        if (!parsedResponse.success) {
-          return { success: false, result: parsedResponse.error.message };
-        }
+                async function getJobResult(id: string) {
+                    const jobResultResponse = await ky
+                        .get(
+                            `${env.WEBSERVER_ADDRESS}/haskell/getResult/${id}`
+                        )
+                        .json();
 
-        return parsedResponse.data;
-      } catch (error) {
-        throw new trpc.TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Could not make request to code runner.",
-          cause: error,
-        });
-      }
-    },
-  })
-  .query("spam", {
-    input: z.object({
-      count: z.number(),
-      code: z.string(),
-      test: z.string(),
-    }),
-    async resolve({ input }) {
-      const timeStart = Date.now();
+                    console.log(jobResultResponse);
 
-      const ps = Array(input.count)
-        .fill(undefined)
-        .map(() => {
-          try {
-            return fetch(`${env.WEBSERVER_ADDRESS}/haskell`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ code: input.code, test: input.test }),
-            }).then((res) => res.json());
-          } catch {
-            return undefined;
-          }
-        });
+                    const parsedResultResponse =
+                        jobResultResponseSchema.safeParse(jobResultResponse);
 
-      const responses = await Promise.all(ps);
-      const fulfilledCount = responses.filter((x) => x !== undefined).length;
-      const duration = Date.now() - timeStart;
+                    if (!parsedResultResponse.success) {
+                        throw new Error(parsedResultResponse.error.message);
+                    }
 
-      return {
-        requestCount: input.count,
-        fulfilled: fulfilledCount,
-        responses,
-        duration,
-      };
-    },
-  });
+                    return parsedResultResponse;
+                }
+
+export const codeRouter = createRouter()
+    .mutation("haskell", {
+        input: z.object({
+            code: z.string(),
+            test: z.string(),
+        }),
+        output: z.object({
+            success: z.boolean(),
+            result: z.string(),
+        }),
+        async resolve({ input }) {
+            try {
+                const jobSubmitResponse = await ky
+                    .post(`${env.WEBSERVER_ADDRESS}/haskell/submit`, {
+                        json: { code: input.code, test: input.test },
+                    })
+                    .json();
+
+                const parsedSubmitResponse =
+                    jobSubmitResponseSchema.safeParse(jobSubmitResponse);
+
+                console.log("Got submit response");
+
+                if (!parsedSubmitResponse.success) {
+                    return {
+                        success: false,
+                        result: parsedSubmitResponse.error.message,
+                    };
+                }
+
+                console.log("Submit response is good");
+
+                for (let i = 0; i < 200; i++) {
+                    const { data } = await getJobResult(parsedSubmitResponse.data.id);
+
+                    if (data.status === "not found") {
+                        return {
+                            success: false,
+                            result: "Job not found.",
+                        };
+                    }
+
+                    if (data.status === "in progress") {
+                        await sleep(100);
+                    }
+
+                    if (data.status === "complete") {
+                        return {
+                            success: data.success,
+                            result: data.output,
+                        };
+                    }
+                }
+
+                throw new trpc.TRPCError({
+                    message: "Failed to get job results.",
+                    code: "INTERNAL_SERVER_ERROR",
+                })
+            } catch (error) {
+                throw new trpc.TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Could not make request to code runner.",
+                    cause: error,
+                });
+            }
+        },
+    })
+    .query("spam", {
+        input: z.object({
+            count: z.number(),
+            code: z.string(),
+            test: z.string(),
+        }),
+        async resolve({ input }) {
+            const timeStart = Date.now();
+
+            const ps = Array(input.count)
+                .fill(undefined)
+                .map(() => {
+                    try {
+                        return fetch(`${env.WEBSERVER_ADDRESS}/haskell`, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                code: input.code,
+                                test: input.test,
+                            }),
+                        }).then((res) => res.json());
+                    } catch {
+                        return undefined;
+                    }
+                });
+
+            const responses = await Promise.all(ps);
+            const fulfilledCount = responses.filter(
+                (x) => x !== undefined
+            ).length;
+            const duration = Date.now() - timeStart;
+
+            return {
+                requestCount: input.count,
+                fulfilled: fulfilledCount,
+                responses,
+                duration,
+            };
+        },
+    });
