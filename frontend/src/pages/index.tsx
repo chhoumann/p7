92c74298a1272970539d6329d5d4025dd7110550
Ivikhostrup <ihostr16@student.aau.dk>
2022-11-03 21:06:51 +0100

    Update

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,9 +7,9 @@ export default function IndexPage() {
     return (
         <Layout title={"Home"}>
             <div className="background-content">
-                <h3 className="landing-title-first link link-underline link-underline-black">Welcome to the AAU</h3>
-                <h3 className="landing-title-second link link-underline link-underline-black">Programming Paradigms</h3>
-                <h3 className="landing-title-third link link-underline link-underline-black">course</h3>
+                <h3 className="landing-title-first link link-underline link-underline-black">Welcome to</h3>
+                <h3 className="landing-title-second link link-underline link-underline-black">The AAU Programming</h3>
+                <h3 className="landing-title-third link link-underline link-underline-black">Paradigms Course</h3>
             </div>
         </Layout>
     );
