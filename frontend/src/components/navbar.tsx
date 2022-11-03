d30afa45eef39d68f3eb4ad887347c1110d5fffb
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-11-03 13:31:02 +0100

    abstract to layout and navbar

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav>
+            <div className="border-b-2 p-4">
+                <span className="font-bold">Home</span>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
