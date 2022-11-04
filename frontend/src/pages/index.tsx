b5654ab9e1624235ba564efbb96aa4fcb009337e
Ivikhostrup <ihostr16@student.aau.dk>
2022-11-04 08:36:47 +0100

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function IndexPage() {
     return (
         <Layout title={"Home"}>
             <div className="background-content">
-                <h3 className="landing-title-first max-sm">Welcome to</h3>
+                <h3 className="landing-title-first max-md">Welcome to</h3>
                 <h3 className="landing-title-second max-sm">The AAU Programming</h3>
                 <h3 className="landing-title-third max-sm">Paradigms Course</h3>
             </div>
