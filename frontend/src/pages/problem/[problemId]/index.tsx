70c4914cd86c77fbf295491c84a5ff38995c9262
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-11-05 18:07:49 +0100

    add submission select for submission version control

--- frontend/src/pages/problem/[problemId]/index.tsx
+++ frontend/src/pages/problem/[problemId]/index.tsx
@@ -1,18 +1,21 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { useState } from "react";
 import { trpc } from "../../../utils/trpc";
 import { useRouter } from "next/router";
 import Layout from "../../../components/layout";
 import CodeEditor from "../../../components/codeEditor";
+import dayjs from "dayjs";
+import { getServerAuthSession } from "../../../server/common/get-server-auth-session";
+import { PrismaClient } from "@prisma/client";
 
 enum TabState {
     Instructions,
     Result,
 }
 
-const SolveProblem: NextPage = () => {
+const SolveProblem: NextPage<{template: string}> = ({ template }) => {
     const [tab, setTab] = useState<TabState>(TabState.Instructions);
-    const [code, setCode] = useState<string>("");
+    const [code, setCode] = useState<string>(template);
     const router = useRouter();
     const { problemId } = router.query;
 
@@ -57,18 +60,24 @@ const SolveProblem: NextPage = () => {
                 <div className="flex flex-row gap-8 justify-center my-auto h-full w-full px-10">
                     <div className="border-2 w-1/2 h-3/4 flex flex-col rounded-lg">
                         <CodeEditor
-                            value={problem.data.template}
+                            value={code}
                             height="100%"
                             className="h-full px-2 py-1 font-mono resize-none rounded-lg outline-0"
                             setCode={setCode}
                         />
                         <div className="w-full p-4 gap-2 items-center justify-end flex flex-row border-t">
+                            <AttemptSelect
+                                problemId={problem.data.id}
+                                setCode={setCode}
+                                defaultCode={template}
+                            />
                             <button
                                 className="rounded-lg bg-green-500 hover:bg-green-400 px-4 py-2 text-white font-semibold"
                                 onClick={() =>
                                     mutation.mutate({
                                         code,
                                         test: test.data.code,
+                                        problemId: problem.data.id,
                                     })
                                 }
                             >
@@ -166,4 +175,82 @@ function Results({ result, success }: { result?: string; success?: boolean }) {
     );
 }
 
+function AttemptSelect({
+    setCode,
+    problemId,
+    defaultCode,
+}: {
+    problemId: string;
+    setCode: (code: string) => void;
+    defaultCode: string;
+}) {
+    const [selected, setSelected] = useState<string>("");
+
+    const submissions = trpc.useQuery(
+        ["submission.byProblemId", { problemId }],
+        {
+            enabled: true,
+        }
+    );
+
+    if (submissions.isLoading) {
+        return null;
+    }
+
+    if (!submissions.isSuccess || submissions.data === undefined) {
+        return null;
+    }
+
+    return (
+        <select
+            className="w-full p-2 rounded-lg"
+            value={selected}
+            onChange={(e) => {
+                setSelected(e.target.value);
+                setCode(e.target.value);
+            }}
+        >
+            <option value={defaultCode}>Select a submission</option>
+            {submissions.data.map((submission, i) => (
+                <option key={submission.id} value={submission.code}>
+                    #{submissions.data.length - i}{" "}
+                    {dayjs(submission.createdAt).format("MMM D, YYYY HH:mm")} -{" "}
+                    {submission.success ? "✅" : "❌"}
+                </option>
+            ))}
+        </select>
+    );
+}
+
 export default SolveProblem;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const session = await getServerAuthSession(ctx);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/login",
+                permanent: false,
+            },
+        };
+    }
+
+    const prisma = new PrismaClient();
+    const { problemId } = ctx.query;
+
+    const problem = await prisma.problem.findUnique({
+        where: {
+            id: problemId as string,
+        },
+        select: {
+            template: true,
+        }
+    });
+
+    return {
+        props: {
+            template: problem?.template
+        },
+    };
+};
