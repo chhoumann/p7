158299a709d30a867a90e6b5381c7b318f864929
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-11-16 11:32:22 +0100

    style tabs

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -59,8 +59,8 @@ const OverviewPage: NextPage = () => {
                                                   "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
                                                   "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
                                                   selected
-                                                      ? "bg-white shadow"
-                                                      : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                                                      ? "bg-white shadow font-bold"
+                                                      : "text-white hover:bg-white/[0.12] font-semibold"
                                               )
                                           }
                                       >
