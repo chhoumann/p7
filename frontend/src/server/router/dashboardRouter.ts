d882c033c4d379be80c62ce81ea418c63af52989
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-11-16 11:32:51 +0100

    filter duplicate submissions by users

--- frontend/src/server/router/dashboardRouter.ts
+++ frontend/src/server/router/dashboardRouter.ts
@@ -10,7 +10,7 @@ export const dashboardRouter = createRouter()
     })
     .query("dashboardData", {
         async resolve({ ctx }) {
-            return await ctx.prisma.problemSet.findMany({
+            const res = await ctx.prisma.problemSet.findMany({
                 select: {
                     topic: true,
                     Problems: {
@@ -23,14 +23,60 @@ export const dashboardRouter = createRouter()
                                         select: {
                                             _count: true,
                                             name: true,
-                                        }
+                                        },
                                     },
                                     success: true,
-                                }
-                            }
-                        }
-                    }
-                }
+                                },
+                            },
+                        },
+                    },
+                },
             });
+
+            const r = res.map((problemSet) => {
+                problemSet.Problems = problemSet.Problems.map((problem) => {
+                    const seen: {
+                        [name: string]: boolean;
+                    } = {};
+
+                    problem.Submission = problem.Submission.filter(
+                        (submission) => {
+                            if (!submission.user || !submission.user.name)
+                                return false;
+
+                            const hasSeenUser = seen.hasOwnProperty(
+                                submission.user.name
+                            );
+
+                            if (hasSeenUser) return false;
+
+                            if (submission.success) {
+                                seen[submission.user.name] = true;
+                                return true;
+                            }
+
+                            const userHasLaterSuccessfulSubmission =
+                                problem.Submission.find(
+                                    (sub) =>
+                                        sub.user.name ===
+                                            submission.user.name && sub.success
+                                );
+
+                            if (!userHasLaterSuccessfulSubmission) {
+                                seen[submission.user.name] = true;
+                                return true;
+                            }
+
+                            return false;
+                        }
+                    );
+
+                    return problem;
+                });
+
+                return problemSet;
+            });
+
+            return r;
         },
     });
