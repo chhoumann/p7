202e3ad538806193855d0ccfad89e9d920941c6e
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2022-12-01 11:44:54 +0100

    get f

--- benchmarks/Benchmarks/RocketBenchmarks.cs
+++ benchmarks/Benchmarks/RocketBenchmarks.cs
@@ -1,4 +1,5 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Text.Json;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Engines;
 using CodeRunnerClients;
 using CodeRunnerClients.DataTransfer;
@@ -10,24 +11,45 @@ namespace Benchmarks;
 [SimpleJob(RunStrategy.Monitoring, launchCount: 5, warmupCount: 0, targetCount: 10)]
 public class RocketBenchmarks
 {
-    [Params(10, 20, 50, 100)]
-    public int NumberOfRequests { get; set; }
-    
+    [Params(10, 20, 50, 100)] public int NumberOfRequests { get; set; }
+
     [ParamsSource(nameof(CodeSubmissions))]
     public CodeSubmission CodeSubmission { get; set; }
-    
+
     public static IEnumerable<CodeSubmission> CodeSubmissions => CodeLoader.Load();
 
     [Benchmark]
     public void PostAndWaitForResponseReceived()
     {
-        IEnumerable<Task> clientActions = TaskBuilder.BuildClientTaskList<CodeRunnerClient>(NumberOfRequests, Action);
+        var clientActions = new List<Task>(NumberOfRequests);
+        List<Task> tasks = new(NumberOfRequests);
+        List<Action> actions = new List<Action>(NumberOfRequests);
+
+        for (int i = 0; i < NumberOfRequests; i++)
+        {
+            CodeRunnerClient client = new();
+
+            actions.Add(async () =>
+            {
+                var post = await client.Post(CodeSubmission);
+                post.EnsureSuccessStatusCode();
+                var result = await JsonSerializer.DeserializeAsync<RocketTestRunResult>(
+                    await post.Content.ReadAsStreamAsync()
+                );
+            });
+        }
+
+        foreach (Action action in actions)
+        {
+            tasks.Add(Task.Run(action));
+        }
+
         Task.WhenAll(clientActions).Wait();
     }
 
     private async void Action(CodeRunnerClient client)
-    { 
-        HttpResponseMessage  response = await client.Post(CodeSubmission);
+    {
+        HttpResponseMessage response = await client.Post(CodeSubmission);
         response.EnsureSuccessStatusCode();
     }
 }
--- benchmarks/Clients/DataTransfer/RequestsAndResponses.cs
+++ benchmarks/Clients/DataTransfer/RequestsAndResponses.cs
@@ -5,3 +5,5 @@ public record CodeSubmission(string code, string test);
 public record PullIdResponse(string id);
 
 public record TestRunResult(string status, bool? success, string? output);
+
+public record RocketTestRunResult(bool? success, string? result);
