2ca0780708321dd9c81d2b5c07cf26b9fa255b2c
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-01-18 07:58:42 +0100

    formatting

--- frontend/src/components/codeEditor.tsx
+++ frontend/src/components/codeEditor.tsx
@@ -8,16 +8,22 @@ import { StreamLanguage } from "@codemirror/language";
 import { haskell } from "@codemirror/legacy-modes/mode/haskell";
 import { EditorView } from "@codemirror/view";
 
-function CodeEditor(
-    {setCode, ...props}: ReactCodeMirrorProps & React.RefAttributes<ReactCodeMirrorRef> & { setCode: (str: string) => void}
-
-) {
+function CodeEditor({
+    setCode,
+    ...props
+}: ReactCodeMirrorProps &
+    React.RefAttributes<ReactCodeMirrorRef> & {
+        setCode: (str: string) => void;
+    }) {
     return (
         <CodeMirror
             theme={githubLight}
-            extensions={[StreamLanguage.define(haskell), EditorView.updateListener.of(e => {
-                setCode(e.state.doc.toString());
-            })]}
+            extensions={[
+                StreamLanguage.define(haskell),
+                EditorView.updateListener.of((e) => {
+                    setCode(e.state.doc.toString());
+                }),
+            ]}
             autoFocus={true}
             style={{ fontSize: "1.2rem" }}
             {...props}
